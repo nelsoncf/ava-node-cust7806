75e5fe842b5f87779d95ff2e0bc8b40ee3083569
Nelson Campos <ncamposfh@gmail.com>
2018-07-21 14:17:36 -0300

    mudanças no config para ler .env

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
+require("dotenv").config()
+
 var objetoCongig = {
-    "CDC_PORT": process.env.CDC_PORT || 3000
+    "CDC_PORT": process.env.CDC_PORT || 3000,
+    "DB_PORT": process.env.DB_PORT || 3306,
 }
 
 module.exports = objetoCongig;
--- routes/produtos.js
+++ routes/produtos.js
@@ -1,19 +1,38 @@
 module.exports = function (server) {
     server.get('/produtos', function(request, response){
-        var livros = [
-            {
-                titulo: "As Aventuras de Rolindo",
-                preco: 50,
-                descricao: "O terror do PHP"
-            },
-            {
-                titulo: "As Aventuras de Rolindo 2",
-                preco: 60,
-                descricao: "O servidor agora é outro"
-            }
-        ]
-        response.render("produtos/lista.ejs", {
-            livros: livros
+        
+        var mysql = require('mysql')
+        var con = mysql.createConnection({
+            database: "cdc",
+            user: "root",
+            password: "",
+            port: 3306
         })
+
+        con.query('SELECT * FROM livros', function (err, livros){
+            con.end()
+            
+            if(!err){
+                response.render("produtos/lista.ejs", {
+                    livros: livros
+                })
+            } else {
+                response.send(err)
+            }
+        });
+
+        
+        // var livros = [
+        //     {
+        //         titulo: "As Aventuras de Rolindo",
+        //         preco: 50,
+        //         descricao: "O terror do PHP"
+        //     },
+        //     {
+        //         titulo: "As Aventuras de Rolindo 2",
+        //         preco: 60,
+        //         descricao: "O servidor agora é outro"
+        //     }
+        // ]
     });
 }
