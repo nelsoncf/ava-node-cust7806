3e75ea89c1a65b67a0a497ea95c62e3580bf8fd3
Nelson Campos <ncamposfh@gmail.com>
2018-07-28 12:16:10 -0300

    aula sabado

--- routes/produtos.js
+++ routes/produtos.js
@@ -1,4 +1,5 @@
 var connectionFactory = require("../db/connectionFactory")
+var queryString = require('query-string')
 
 function pegaLivros(conexao){
     return new Promise( (resolve, reject) => {
@@ -13,11 +14,41 @@ function pegaLivros(conexao){
 }   
 
 module.exports = function (server) {
-    server.get('/produtos', function(request, response){
+    server.get('/produtos', async function(request, response){
     
-        connectionFactory.getConnection()
-            .then(conexao => pegaLivros(conexao))
-            .then(livros => response.render("produtos/lista.ejs", { livros: livros }))
-            .catch(err => response.send(err.message))
+        // connectionFactory.getConnection()
+        //     .then(conexao => pegaLivros(conexao))
+        //     .then(livros => response.render("produtos/lista.ejs", { livros: livros }))
+        //     .catch(err => response.send(err.message))
+
+        try {
+            var conexao = await connectionFactory.getConnection()
+            var livros = await pegaLivros(conexao)
+            response.render("produtos/lista.ejs", { livros: livros })
+        } catch (err) {
+            response.send(err.message)
+        }
     });
+
+    server.get('/produtos/form', function(req, res){
+        res.render("produtos/form.ejs", { validationErrors: [] })
+    })
+
+    server.post('/produtos', async function(req, res){
+        var livro = req.body
+        
+        
+        req.assert("titulo", "Titulo inválido").notEmpty()
+        req.assert("preco", "Preço inválido").notEmpty()
+        req.assert("preco", "Preço obrigatório").isNumeric()
+
+        try{
+            await req.asyncValidationErrors()
+            res.redirect('/produtos')
+        } catch(validationErrors) {
+            res.render('produtos/form.ejs', {
+                validationErrors
+            })
+        }
+    })
 }
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require("express")
 var config = require("./config")
 
 var server = express()
+var expressValidator = require('express-validator')
+
+server.use(expressValidator())
+server.use(express.urlencoded())
+server.use(express.json())
 
 server.get('/', function(request, response){
     response.render("index.ejs")
